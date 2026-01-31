3c8ad526ddfc65be892b560b614678197decfb6b
Soufiane Amajat <soufianeamt11@gmail.com>
2026-01-31 13:16:04 +0100

    chore : add new test clip

--- apps/app/data/sample-videos.ts
+++ apps/app/data/sample-videos.ts
@@ -1,10 +1,11 @@
 export const SAMPLE_VIDEOS = [
-  { id: "1", url: "https://auth.reelty.io/storage/v1/object/public/output/007f4411-6d7f-474e-8ddc-616f166cf3df/1751838425660.mp4", thumbnail: "", duration: 15 },
-  { id: "2", url: "https://auth.reelty.io/storage/v1/object/public/output/153d87b6-c0d2-4dcc-b200-8011ef6e81fc/1752837451258.mp4", thumbnail: "", duration: 15 },
-  { id: "3", url: "https://auth.reelty.io/storage/v1/object/public/output/1678c00b-ac00-4a6e-adf3-d78eff93db9f/1753319625888.mp4", thumbnail: "", duration: 15 },
-  { id: "4", url: "https://auth.reelty.io/storage/v1/object/public/output/1678c00b-ac00-4a6e-adf3-d78eff93db9f/1753319622768.mp4", thumbnail: "", duration: 15 },
-  { id: "5", url: "https://auth.reelty.io/storage/v1/object/public/output/1678c00b-ac00-4a6e-adf3-d78eff93db9f/1753319742803.mp4", thumbnail: "", duration: 15 },
-  { id: "6", url: "https://auth.reelty.io/storage/v1/object/public/output/8ca57bf5-84c3-4b37-a29c-4afc8793869d/1751123895329.mp4", thumbnail: "", duration: 15 },
+    // { id: "1", url: "/videos/clip7.mp4", thumbnail: "", duration: 12 },
+  { id: "1", url: "https://auth.reelty.io/storage/v1/object/public/output/007f4411-6d7f-474e-8ddc-616f166cf3df/1751838425660.mp4", thumbnail: "", duration: 5 },
+  { id: "2", url: "https://auth.reelty.io/storage/v1/object/public/output/153d87b6-c0d2-4dcc-b200-8011ef6e81fc/1752837451258.mp4", thumbnail: "", duration: 5 },
+  { id: "3", url: "https://auth.reelty.io/storage/v1/object/public/output/1678c00b-ac00-4a6e-adf3-d78eff93db9f/1753319625888.mp4", thumbnail: "", duration: 5 },
+  { id: "4", url: "https://auth.reelty.io/storage/v1/object/public/output/1678c00b-ac00-4a6e-adf3-d78eff93db9f/1753319622768.mp4", thumbnail: "", duration: 5 },
+  { id: "5", url: "https://auth.reelty.io/storage/v1/object/public/output/1678c00b-ac00-4a6e-adf3-d78eff93db9f/1753319742803.mp4", thumbnail: "", duration: 5 },
+  { id: "6", url: "https://auth.reelty.io/storage/v1/object/public/output/8ca57bf5-84c3-4b37-a29c-4afc8793869d/1751123895329.mp4", thumbnail: "", duration: 5 },
 ];
 
 export { GAP_BETWEEN_CLIPS, getClipWidth, getConstrainedHeight } from "./constants";
